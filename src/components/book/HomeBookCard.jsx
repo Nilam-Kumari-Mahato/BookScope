7be6aa7b47f3683c8701ab67964a3fa3897d59f7
Nilam Kumari Mahato <nilamkumarimahato10@gmail.com>
2026-01-18 08:24:50 +0530

    ui changes

--- src/components/book/HomeBookCard.jsx
+++ src/components/book/HomeBookCard.jsx
@@ -13,7 +13,7 @@ export default function HomeBookCard( {book} ) {
         <div 
             onClick={handleclick}
             key={book.id} 
-            className="flex flex-col gap-4 justify-arround m-2 p-4 bg-amber-800/20 rounded-lg cursor-pointer transtion-all ease-in-out duration-200 hover:shadow-amber-950"
+            className="flex flex-col gap-4 justify-arround m-2 p-4 bg-amber-800/20 rounded-lg cursor-pointer transtion-all ease-in-out duration-200 hover:shadow-amber-950 shadow-lg"
         >
             <div>
                 <img src={book.thumbnail} alt="book cover" className="w-20 h-25 md:w-40 md:h-45 transition-all ease-in-out duration-200 hover:scale-105" />
